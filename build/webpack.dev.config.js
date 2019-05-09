6060b0b570f27049696251edc02c14f43960c9f5
robin <lb.robin1991@gmail.com>
2019-05-09 11:15:11 +0800

    add webpack dev & example build

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const webpackBaseConfig = require('./webpack.base.config.js');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+
+
+module.exports = merge(webpackBaseConfig, {
+    devtool: 'eval-source-map',
+    entry: {
+        main: './examples/main',
+        vendors: ['vue', 'vue-router']
+    },
+    output: {
+        path: path.join(__dirname, '../examples/dist'),
+        publicPath: '',
+        filename: '[name].js',
+        chunkFilename: '[name].chunk.js'
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm.js'
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'async',
+            minSize: 30000,
+            maxSize: 0,
+            minChunks: 1,
+            automaticNameDelimiter: '~',
+            name: true,
+        }
+    },
+    plugins: [
+        // new webpack.optimize.CommonsChunkPlugin({ name: 'vendors', filename: 'vendor.bundle.js' }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: path.join(__dirname, '../examples/dist/index.html'),
+            template: path.join(__dirname, '../examples/index.html')
+        }),
+        new FriendlyErrorsPlugin()
+    ]
+});
