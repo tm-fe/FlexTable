e0d96a18b3631cb4cf1944a08850798b87f35135
robin <lb.robin1991@gmail.com>
2019-05-16 17:20:09 +0800

    add codecov

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -19,6 +19,8 @@ module.exports = function(config) {
             reporters: [
                 { type: 'lcov', subdir: '.' },
                 { type: 'text-summary' },
+                { type: 'cobertura', subdir: '.' },
+                { type: 'json', subdir: '.' },
             ],
         },
         client: {
