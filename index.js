90a93d8f3f0820e8c68b584ab5e72db4971413e2
robin <lb.robin1991@gmail.com>
2019-05-20 20:30:39 +0800

    feat: 完善引入方式 demo & readme

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const install = function (Vue) {
     Vue.component('flex-table', FlexTable);
 };
 
+if (typeof window !== 'undefined' && window.Vue) {
+    install(window.Vue);
+}
+
 export default { install, FlexTable };
 export { install, FlexTable };
