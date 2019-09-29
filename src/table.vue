b0f359a7e2b6421812661f06383061772e73bb4c
robin <lb.robin1991@gmail.com>
2019-09-29 14:36:08 +0800

    feat: 优化滚动事件，考虑表格无内滚的情况

--- src/table.vue
+++ src/table.vue
@@ -349,8 +349,14 @@ export default {
         handleMousewheel(event) {
             const normalized = normalizeWheel(event);
             if (Math.abs(normalized.spinY) > 0) {
-                event.preventDefault();
-                this.scrollTop += Math.ceil(normalized.pixelY);
+                const bodyWrapper = this.$refs.tableBody.$el;
+                const currentScrollTop = this.scrollTop;
+                const noYetScrollToTop = normalized.pixelY < 0 && currentScrollTop !== 0;
+                const noYetScrollToBottom = normalized.pixelY > 0 && bodyWrapper.scrollHeight - bodyWrapper.clientHeight > currentScrollTop;
+                if (noYetScrollToTop || noYetScrollToBottom) {
+                    event.preventDefault();
+                    this.scrollTop += Math.ceil(normalized.pixelY);
+                }
             }
         },
         doLayout: debounce(function() {
