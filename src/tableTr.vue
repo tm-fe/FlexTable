595210bd6c3079ae09843f7e246297c0deb76258
guoyun.zhang <guoyun.zhang@global.nativex.com>
2021-09-26 19:30:06 +0800

    fix: 修复行不对齐的问题

--- src/tableTr.vue
+++ src/tableTr.vue
@@ -81,7 +81,7 @@ export default {
         this.onRowHeightChange();
     },
     updated() {
-        this.$nextTick(() => {
+        requestAnimationFrame(() => {
             this.onRowHeightChange();
         });
     },
